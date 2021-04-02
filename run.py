d17080e9367a94eaadb13787ea8949821daecff2
Julian Merida <julianmr97@gmail.com>
2021-04-01 22:19:16 -0300

    Agregar \n en param_gcc para poder modificar el makefile

--- run.py
+++ run.py
@@ -31,17 +31,17 @@ def edit_make(compiler, flag):
 
 
 paramgcc = [
-        "CFLAGS  = -O0",
-        "CFLAGS  = -O1",
-        "CFLAGS  = -O2",
-        "CFLAGS  = -O2 -march=native",
-        "CFLAGS  = -O3",
-        "CFLAGS  = -O3 -march=native",
-        "CFLAGS  = -O3 -ffast-math",
-        "CFLAGS  = -O3 -funroll-loops",
-        "CFLAGS  = -O3 -funswitch-loops",
-        "CFLAGS  = -O3 -march=native -DN=512",
-        "CFLAGS  = -O3 -march=native -DN=1024",
+        "CFLAGS  = -O0\n",
+        "CFLAGS  = -O1\n",
+        "CFLAGS  = -O2\n",
+        "CFLAGS  = -O2 -march=native\n",
+        "CFLAGS  = -O3\n",
+        "CFLAGS  = -O3 -march=native\n",
+        "CFLAGS  = -O3 -ffast-math\n",
+        "CFLAGS  = -O3 -funroll-loops\n",
+        "CFLAGS  = -O3 -funswitch-loops\n",
+        "CFLAGS  = -O3 -march=native -DN=512\n",
+        "CFLAGS  = -O3 -march=native -DN=1024\n",
 ]
 param_icc = [
         "CFLAGS  = -O0\n",

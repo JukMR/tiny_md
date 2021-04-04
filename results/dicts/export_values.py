1b8f058ddfb0f69bdb58a0df9e31c377511233fe
Julian Merida <julianmr97@gmail.com>
2021-04-04 11:46:28 -0300

    Arreglar argumentos en export_values.py

--- results/dicts/export_values.py
+++ results/dicts/export_values.py
@@ -1,7 +1,4 @@
-from clang_values import clang
-from gcc_values import gcc
-from gcc_10_values import gcc_10
-from icc_values import icc
+import sys
 
 
 def export_values(array):
@@ -11,4 +8,18 @@ def export_values(array):
         print(f"")
 
 
-export_values(clang)
+# Pass values to export as command argument
+set = sys.argv[1]
+
+if set == 'clang':
+    from clang_values import clang
+    export_values(clang)
+elif set == 'gcc':
+    from gcc_values import gcc
+    export_values(gcc)
+elif set == 'gcc_10':
+    from gcc_10_values import gcc_10
+    export_values(gcc_10)
+elif set == 'icc':
+    from icc_values import icc
+    export_values(icc)

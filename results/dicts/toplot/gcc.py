fcd1876567a5386c2768413b54e5861fc7ac8e68
Julian Merida <julianmr97@gmail.com>
2021-04-10 16:57:23 -0300

    Arreglar reglas PEP8 en results/dicts/toplot/gcc.py

--- results/dicts/toplot/gcc.py
+++ results/dicts/toplot/gcc.py
@@ -1,57 +1,62 @@
-#f = open('gcc.plot')
-#f.readlines()
-#import csv
+# f = open('gcc.plot')
+# f.readlines()
+# import csv
 import matplotlib.pyplot as plt
 import numpy as np
 from scipy.stats import norm
+
+
 def gaussian(x, mu, sig):
     return np.exp(-np.power((x - mu)/sig, 2.)/2)
-plt.figure(1)  
+
+
+plt.figure(1)
 with open('gcc.plot') as f:
-    header=next(f)
+    header = next(f)
     print(header)
-    numlines=0
-    gflops=[]
-    cflags=[]
+    numlines = 0
+    gflops = []
+    cflags = []
     for line in f:
-        #spliteo con ',' y borro espacios en blanco
+        # spliteo con ',' y borro espacios en blanco
         v = [x.strip() for x in line.split(',')]
-        #v = line.split(',') #old
-        print (v[0],v[1],v[2],v[3])
+        # v = line.split(',') # old
+        print(v[0], v[1], v[2], v[3])
         gflops.append(float(v[3]))
         cflags.append(v[0])
-        #fig = plt.figure()
-        time=np.linspace(0, 20,10000)
-        #rv = norm(loc = float(v[1]), scale = float(v[2]))
-        #plt.plot(time, rv.pdf(time)	)  
-        plt.plot(time,gaussian(time,float(v[1]),float(v[2])),label=v[0])
+        # fig = plt.figure()
+        time = np.linspace(0, 20, 10000)
+        # rv = norm(loc = float(v[1]), scale = float(v[2]))
+        # plt.plot(time, rv.pdf(time)	)
+        plt.plot(time, gaussian(time, float(v[1]), float(v[2])), label=v[0])
         plt.legend(loc=2, prop={'size': 6})
-        numlines=numlines+1
-        #plt.legend(loc="upper left")
-        #, label=v[0])
+        numlines = numlines + 1
+        # plt.legend(loc="upper left")
+        # , label=v[0])
 plt.title('Comparación de tiempos para diferentes CFLAGS. Compilador gcc 9.4')
 plt.xlabel('tiempo [s]')
 plt.ylabel('distribución gaussiana (normalizada)')
 plt.show()
 
-## AHORA VAMOS A GRAFICAR LOS GFLOPS
+# AHORA VAMOS A GRAFICAR LOS GFLOPS
 plt.figure(2)   # creamos nueva fig
-#with open('gcc.plot') as f:
-#    header=next(f)
-#    for line in f:
-        #spliteo con ',' y borro espacios en blanco
-#        v = [x.strip() for x in line.split(',')]
-        #v = line.split(',') #old
-        #fig = plt.figure()
-        #rv = norm(loc = float(v[1]), scale = float(v[2]))
-        #plt.plot(time, rv.pdf(time)	)  
+# with open('gcc.plot') as f:
+#     header = next(f)
+#     for line in f:
+#         spliteo con ',' y borro espacios en blanco
+#         v = [x.strip() for x in line.split(',')]
+#         v = line.split(',')  # old
+#         fig = plt.figure()
+#         rv = norm(loc=float(v[1]), scale=float(v[2]))
+#         plt.plot(time, rv.pdf(time)	)
 y_pos = np.arange(numlines)
 plt.bar(y_pos, gflops)
 plt.subplots_adjust(bottom=0.5, top=0.9)
-        # Create names on the x-axis
-plt.xticks(y_pos, cflags,rotation=90)
-        
-        # Show graphic
+
+# Create names on the x-axis
+plt.xticks(y_pos, cflags, rotation=90)
+
+# Show graphic
 plt.title('Comparación de GFLOPS para diferentes CFLAGS. Compilador gcc 9.4')
 plt.xlabel('CFLAGS')
 plt.ylabel('GFLOPS')

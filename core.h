935c23e180956fdc4ca933bc681287d068dbce47
Julian Merida <julianmr97@gmail.com>
2021-05-06 21:53:31 -0300

    Modificar epot y pres como arreglos

--- core.h
+++ core.h
@@ -5,9 +5,6 @@ void init_pos(double* rx, double* ry, double* rz, const double rho);
 
 void init_vel(double* vx, double* vy, double* vz, double* temp, double* ekin);
 
-extern void forces(const double* rx, const double* ry, const double* rz,
-                   double* fx, double* fy, double* fz, double* epot, double* pres, const double* temp, const double rho, const double V, const double L);
-
 void velocity_verlet(double* rx, double* ry, double* rz, double* vx,
                      double* vy, double* vz,
                      double* fx, double* fy, double* fz, double* epot,
